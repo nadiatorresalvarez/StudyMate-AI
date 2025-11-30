7d14471d346cb9efd1039a18beb2b2f2510a9cae
carlosalonsomamaniccollque <carlosalonzomamaniccollque@gmail.com>
2025-11-30 11:23:45 -0500

    Integración Gemini funcionando y corrección de API key

--- StudyMateAI.Application/Common/Abstractions/IFileStorage.cs
+++ StudyMateAI.Application/Common/Abstractions/IFileStorage.cs
@@ -5,4 +5,5 @@ namespace StudyMateAI.Application.Common.Abstractions;
 public interface IFileStorage
 {
     Task<string> SaveAsync(Stream stream, string fileName, string contentType, string relativePath, CancellationToken ct);
+    Task DeleteAsync(string fileUrlOrPath, CancellationToken ct);
 }
